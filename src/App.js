d19c0ca2f02a691f9eb9249cca71d1dfe4cf3838
Shashank Bheeshva Gmail <bheeshvashashank@gmail.com>
2025-11-10 10:46:39 -0600

    Dev deploy test App.js
    Dev deploy test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World by Shashank React App';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value} <h1> - React basic – Dev deploy test</h1> </div>;
 }
 
 export default App;
